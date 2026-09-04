4d5fc8d12a586a24d95567b7c3856286c32ba156
dokulamohitkumar-blip <dokulamohitkumar@gmail.com>
2026-09-04 21:10:45 +0530

    Update portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -278,7 +278,7 @@ className={`origin-top-left w-[540px] ${
     ? "scale-[0.55]"
     : project.title === "Distance Monitoring System"
     ? "scale-[0.55]"
-    : "scale-[0.55]"
+    : "scale-[0.53]"
     
 }`}
 >
