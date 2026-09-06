86bbbb20d1014bccd7d760f85c5875aaf1f6246d
dokulamohitkumar-blip <dokulamohitkumar@gmail.com>
2026-09-06 09:20:26 +0530

    Fix mobile project animation width

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -271,7 +271,7 @@ transition-all duration-300`}
 }`}>
 
 <div
-className={`origin-top-left w-[500px] ${
+className={`origin-top-left w-full ${
   project.title === "Industry Parameters Monitoring 4.0"
     ? "scale-[0.59]"
     : project.title === "IoT-Based Smart Parking System"
